538dbc11241103c135d839c62a567b2b5c2d6f32
Yevhen-Kryvdiuk <krivachacha@gmail.com>
2019-05-21 14:31:12 +0300

    considered all the recommendations

--- script.js
+++ script.js
@@ -1,48 +1,51 @@
 const table = document.querySelector('table');
 const tableHead = table.rows[0];
 
-tableHead.onclick = function (event) {
-    const target = event.target;
-    if (target.nodeName === 'TH' ) {
-        const sortParam = target.cellIndex;
-        const data = sortTableData(table,sortParam);
+tableHead.addEventListener('click', function (event) {
+    const {cellIndex: sortParam, nodeName} = event.target;
+    if (nodeName === 'TH') {
+        const data = sortTableData(table, sortParam);
         changeTableData(table, data);
     }
-};
+});
 
 function getTableData(table) {
-    const tableRows = table.rows;
+    const tableRows = [...table.rows];
     const data = [];
     for (let i = 1; i < tableRows.length; i++) {
-        data[i-1] = [tableRows[i].cells[0].innerHTML, tableRows[i].cells[1].innerHTML];
+        const tableRow = tableRows[i];
+        data[i-1] = [tableRow.cells[0].innerHTML, tableRow.cells[1].innerHTML];
     }
-    return data
+    return data;
 }
 
 
-function sortTableData(table,sortParam) {
+function sortTableData(table, sortParam) {
     const data = getTableData(table);
     if (sortParam === 0) {
         data.sort((a, b) => {
-            return a[sortParam] - b[sortParam];
+            return Number(a[sortParam]) - Number(b[sortParam]);
         });
     } else {
         data.sort((a, b) => {
             if (a[sortParam] > b[sortParam]) {
-                return 1; }
+                return 1;
+            }
             if (a[sortParam] < b[sortParam]) {
-                return -1; }
+                return -1;
+                }
             return 0;
         });
     }
     return data;
 }
 function changeTableData(table, data) {
-    let tableRows = table.rows;
+    const tableRows = [...table.rows];
     for (let i = 0; i < data.length; i++) {
         const j = i + 1;
         const tableRow = tableRows[j];
         const dataRow = data[i];
-        [tableRow.cells[0].innerHTML, tableRow.cells[1].innerHTML] = [dataRow[0], dataRow[1]];
+        tableRow.cells[0].innerText = dataRow[0];
+        tableRow.cells[1].innerText = dataRow[1];
     }
 }
